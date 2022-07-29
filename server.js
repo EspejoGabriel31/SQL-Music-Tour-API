4ebc26987bdc3701e7d2e0d52bb6e527d43fbd62
unknown <gespejo2144@gmail.com>
2022-07-28 18:54:44 -0700

    added band model, set up sequelize

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const { Sequelize } = require('sequelize')
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize(process.env.PG_URI)
+// const sequelize = new Sequelize({
+//     storage: process.env.PG_URI,
+//     dialect: 'postgres',
+//     username: 'postgres',
+//     password: 'Highway774'
+// })
+
+try {
+    sequelize.authenticate()
+    console.log(`Connected with Sequelize at ${process.env.PG_URI}`)
+} catch(err){
+    console.log(`Unable to connect to PG: ${err}`)
+}
 // ROOT
 app.get('/', (req, res) => {
     res.status(200).json({
@@ -18,3 +34,4 @@ app.get('/', (req, res) => {
 app.listen(process.env.PORT, () => {
     console.log(`🎸 Rockin' on port: ${process.env.PORT}`)
 })
+
