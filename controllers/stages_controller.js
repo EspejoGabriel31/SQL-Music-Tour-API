5179f76332d5723668a4408646c8f640d5bf4ad4
unknown <gespejo2144@gmail.com>
2022-08-12 22:37:42 -0700

    added remaining associations

--- controllers/stages_controller.js
+++ controllers/stages_controller.js
@@ -1,7 +1,7 @@
 // DEPENDENCIES
 const stages = require('express').Router()
 const db = require('../models')
-const { Stage } = db
+const { Stage, Event } = db
 const { Op } = require('sequelize')
 
 
@@ -21,10 +21,15 @@ stages.get('/', async (req, res) => {
 })
 
 // FIND A SPECIFIC STAGE
-stages.get('/:id', async (req, res) => {
+stages.get('/:name', async (req, res) => {
     try{
         const foundStage = await Stage.findOne({
-            where: {stage_id: req.params.id }
+            where: {name: req.params.name },
+            include: {
+                model: Event,
+                as: "event",
+                through: { attributes: [] }
+            }
         })
         res.status(200).json(foundStage)
     } catch(err){
